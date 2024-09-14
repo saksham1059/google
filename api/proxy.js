4bf5e11d4f8a4b5e4378c14fbc21e1144af9e75d
saksham1059 <89568394+saksham1059@users.noreply.github.com>
2024-09-14 13:21:52 +0530

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://player.autoembed.cc";//your website url
+  let target = "https://player.vidsrc.nl";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
